528f2fa3d36247fe0dc9224e4b9e4b4882742cc4
Mikachu2333 <mikachu.23333@zohomail.com>
2023-12-23 16:51:25 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,41 @@
+#include "locale.h"
+#include "wchar.h"
+#include <filesystem>
+#include <iostream>
+#include <regex>
+#include <string>
+#include <windows.h>
+using namespace std;
+using namespace filesystem;
+
+int main() {
+  /*Set local of the rigion to avoid error caused by wrong coding*/
+  std::setlocale(LC_ALL, "en_US.utf8");
+  /*Init file path*/
+  path file_dir = L"(D:\\LanguageLearning\\python\\music)";
+  if (!exists(file_dir))
+    exit(1);
+  /*Start regex*/
+  wsmatch regex_result_group;
+  wregex reg(L"(.*\\\\)(.*) - (.*)( \\[.*\\])(\\.)(ogg|mp3|flac|wav)");
+  /*Start to enum the dir*/
+  directory_iterator list(file_dir);
+  for (auto &it : list) {
+    wstring temp = it.path().wstring();
+    /*if path could match the regex, set the new_name*/
+    if (regex_search(temp, regex_result_group, reg)) {
+      wstring result = regex_result_group[1].str() +
+                       regex_result_group[3].str() + L"." +
+                       regex_result_group[6].str();
+      wstring original_name = regex_result_group[0].str();
+      wstring brief_name = regex_result_group[3].str();
+      /*Try to rename*/
+      if (0 != _wrename(original_name.c_str(), result.c_str())) {
+        wcout << "Error! " << brief_name << endl;
+      } else {
+        wcout << "Successfully! " << brief_name << endl << endl;
+      }
+    }
+  }
+  return 0;
+}
